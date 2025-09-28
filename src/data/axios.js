4ab47cf87a32da41f1d91961813028b08766742e
usernamedinesh <borod9200@gmail.com>
2025-09-29 00:14:16 +0530

    add backend base url

--- src/data/axios.js
+++ src/data/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api", // Replace with your actual API base URL
-    headers: {
-        "Content-Type": "application/json",
-        // Add other default headers here, e.g. Authorization
-    },
-    timeout: 10000,
+  baseURL: "https://notes-bakend-5edl.onrender.com/api", // Replace with your actual API base URL
+  headers: {
+    "Content-Type": "application/json",
+    // Add other default headers here, e.g. Authorization
+  },
+  timeout: 10000,
 });
 
 export default axiosInstance;
